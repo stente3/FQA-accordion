e34ce1ff2daa8c0af6e7ee9c235e00f5d75ac8b2
stente3 <stentem0103@gmail.com>
2021-08-25 07:20:34 -0500

    Added finishing touches

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,9 +8,11 @@ heading.forEach((element) => {
 	element.addEventListener("click", () => {
 		paragraph.forEach((elemento, i) => {
 			paragraph[i].classList.remove("active");
+			heading[i].classList.remove("active__heading");
 			arrow[i].classList.remove("active__arrow");
 		});
 		element.nextElementSibling.nextElementSibling.classList.add("active");
+		element.classList.add("active__heading");
 		element.nextElementSibling.children[0].classList.add("active__arrow");
 	});
 });
